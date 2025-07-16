a600899b2fc2b6d0f7de3af6c46da541ea8b1cf4
Андрей Сидорченко <andreisidorcenko@MacBook-Pro-Andrej.local>
2025-07-16 15:43:44 +0300

    refactor: replace hardcoded routes with centralized ROUTES constants

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { TaskDetails } from './components/TaskDetails/TaskDetails.tsx';
+import { ROUTES } from './routes';
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/task/:id" element={<TaskDetails />} />
+      <Route path={ROUTES.HOME} element={<Home />} />
+      <Route path={ROUTES.TASK()} element={<TaskDetails />} />
     </Routes>
   );
 }
