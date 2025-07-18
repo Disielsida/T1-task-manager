c6773c7865fcb16bbb62e83a625bf5196d66d01d
Андрей Сидорченко <andreisidorcenko@MacBook-Pro-Andrej.local>
2025-07-18 19:05:24 +0300

    refactor: mobile select double rendering

--- eslint.config.js
+++ eslint.config.js
@@ -44,6 +44,7 @@ export default tseslint.config([
       'react/jsx-uses-react': 'off',
       'react/react-in-jsx-scope': 'off',
       'prettier/prettier': 'warn',
+      "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }]
     },
   }
 ])
